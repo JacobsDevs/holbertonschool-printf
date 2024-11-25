8965fc3580a8f8b8c4fddc25ece860b97dea00fa
mpesaran <mahsa.pesaran@gmail.com>
2024-11-26 10:03:12 +1100

    Delete whitespaces and replace with tab

--- printf.c
+++ printf.c
@@ -8,44 +8,44 @@
  */
 int _printf(const char *format, ...)
 {
-        int i = 0;
-        int len = 0;
-        int (*func)(va_list);
-        va_list ap;
+	int i = 0;
+	int len = 0;
+	int (*func)(va_list);
+	va_list ap;
 
-        if (format == NULL)
-                return (-1);
-        va_start(ap, format);
-        while (format[i] != '\0')
-        {
-                if (format[i] != '%')
-                {
-                        _putchar(format[i]);
-                        len += 1;
-                }
-                else
-                {
-                        if (format[i + 1] == '\0')
-                        {
-                                _putchar('%');
-                                len += 1;
-                                break;
-                        }
-                        func = get_specifier(format[i + 1]);
-                        if (func == NULL)
-                        {
-                                _putchar('%');
-                                _putchar(format[i + 1]);
-                                len += 2;
-                        }
-                        else
-                        {
-                                len += func(ap);
-                        }
-                        i++;
-                }
-                i++;
-        }
-        va_end(ap);
-        return (len);
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			len += 1;
+		}
+		else
+		{
+			if (format[i + 1] == '\0')
+			{
+				_putchar('%');
+				len += 1;
+				break;
+			}
+			func = get_specifier(format[i + 1]);
+			if (func == NULL)
+			{
+				_putchar('%');
+				_putchar(format[i + 1]);
+				len += 2;
+			}
+			else
+			{
+				len += func(ap);
+			}
+			i++;
+		}
+		i++;
+	}
+	va_end(ap);
+	return (len);
 }
